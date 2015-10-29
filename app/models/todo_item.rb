286b774a83cda02581117b220b5f88ff0ceada0b
Travis Smith <travis.smith@mac.com>
2015-10-29 23:10:38 +0000

    add complete/incomplete todo item scope

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -8,4 +8,7 @@ class TodoItem < ActiveRecord::Base
     !completed_at.nil?
   end
   
+  scope :complete, -> { where.not(completed_at: nil) }
+  scope :incomplete, -> { where(completed_at: nil) }
+  
 end
