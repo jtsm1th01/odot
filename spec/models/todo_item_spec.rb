ebf3ed0bc09f41d5df8916fdac97447a885c5ac7
Travis Smith <travis.smith@mac.com>
2015-10-29 22:12:08 +0000

    basic formatting of todo list index

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe TodoItem do
   it { should belong_to(:todo_list) }
+  
+  describe "#completed?" do
+    let(:todo_item) { TodoItem.create(content: "Hello.") }
+    
+    it "is false when completed_at is blank" do
+      todo_item.completed_at = nil
+      expect(todo_item.completed?).to be_false
+    end
+    
+    it "is true when completed_at is not blank" do
+      todo_item.completed_at = 5.minutes.ago
+      expect(todo_item.completed?).to be_true
+    end
+    
+  end
+  
 end
